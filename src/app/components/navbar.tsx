9e2faf4e2949c2f774c5fd0cec104e0181a170f1
balajiRRK <rbalajirrk@gmail.com>
2025-03-16 21:16:47 -0400

    Added css to underline navbar options and highlight which page the user is currently on.

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -5,11 +5,13 @@ import Image from "next/image"
 import { usePathname } from "next/navigation"
 
 export const Navbar = () => {
+    const pathName = usePathname();
+
     return (
         // <nav className="flex items-center justify-between p-4 bg-stone-600">
         <nav className="flex items-center justify-between p-4 bg-transparent absolute top-0 left-0 w-full z-10">
 
-            <Link href="/" className="ml-60">
+            <Link href="/" className="">
                 <Image
                     src="/travellogo.png"
                     alt="Logo"
@@ -20,10 +22,20 @@ export const Navbar = () => {
             </Link>
 
             <div className="flex space-x-4 mr-20">
-                <Link href="/" className="[text-shadow:_0_4px_4px_rgb(0_0_0_/_0.8)] text-white opacity-90 font-semibold text-[25px] mr-10">
+                <Link
+                    href="/"
+                    className={`relative [text-shadow:_0_4px_4px_rgb(0_0_0_/_0.8)] ${
+                        pathName === "/" ? "text-orange-500 after:w-full" : "text-white after:w-0"
+                    } opacity-90 font-semibold text-[25px] mr-10 transition duration-300 hover:after:w-full after:absolute after:left-0 after:-bottom-1 after:h-[2px] after:bg-orange-500 after:transition-all after:duration-300`}
+                >
                     Home
                 </Link>
-                <Link href="/" className="[text-shadow:_0_4px_4px_rgb(0_0_0_/_0.8)] text-white opacity-90 font-semibold text-[25px] mr-40">
+                <Link
+                    href="/explore"
+                    className={`relative [text-shadow:_0_4px_4px_rgb(0_0_0_/_0.8)] ${
+                        pathName === "/explore" ? "text-orange-500 after:w-full" : "text-white after:w-0"
+                    } opacity-90 font-semibold text-[25px] mr-40 transition duration-300 hover:after:w-full after:absolute after:left-0 after:-bottom-1 after:h-[2px] after:bg-white after:transition-all after:duration-300`}
+                >
                     Explore
                 </Link>
             </div>
