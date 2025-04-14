3ecd8d1716f3305b830e53fc0fbb65e2486fac42
balajiRRK <rbalajirrk@gmail.com>
2025-04-13 20:55:51 -0400

    WIP of Explore page

---
+++ src/app/explore/page.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import React from 'react';
+import { USAMap, USAStateAbbreviation } from '@mirawision/usa-map-react';
+
+// const Explore = () => (
+//   <div>
+//     <USAMap customStates={mapSettings}/>
+//   </div>
+// );
+
+// export default Explore;
+
+const handleStateClick = (stateAbbreviation: USAStateAbbreviation) => {
+    
+};
+
+const Explore = () => (
+  <div>
+    <USAMap />
+  </div>
+);
+
+export default Explore;
